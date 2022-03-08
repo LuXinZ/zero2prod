98a435f9593f64766c6cbda38a7f2a665410d387
Lx <zhangluxin1995@gmail.com>
2022-03-09 00:04:14 +0800

    fix

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,12 +1,63 @@
+use std::net::TcpListener;
+
 // !tests/health_check.rs
 #[tokio::test]
-async fn health_check_works()  {
-    spawn_app().await.expect("Failed to spawn our app ");
+async fn health_check_works() {
+    let address = spawn_app();
     let client = reqwest::Client::new();
-    let response = client.get("http://127.0.0.1:8000/health_check").send().await.expect("fail to execute request ");
+    let response = client
+        .get(&format!("{}/health_check", &address))
+        .send()
+        .await
+        .expect("fail to execute request ");
     assert!(response.status().is_success());
-    assert_eq!(Some(0),response.content_length() );
+    assert_eq!(Some(0), response.content_length());
 }
-async fn spawn_app() -> std::io::Result<()> {
-    zero2prod::run().await 
+#[tokio::test]
+async fn subscribe_retruns_a_200_for_valid_from_data() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("failed to execute request");
+    assert_eq!(200, response.status().as_u16());
+}
+#[tokio::test]
+async fn subscribe_retruns_a_400_when_data_is_missing() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("failed to execute request");
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The apid id not fail with 400 bad requet when the payload was {}",
+            error_message
+        )
+    }
+     
+}
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind random port");
+
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("faile to bind address ");
+    let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
