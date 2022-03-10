3942b750d5847c0559279050daa684672d5e4eb5
Lx <zhangluxin1995@gmail.com>
2022-03-10 23:15:10 +0800

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,15 @@
+pub mod configuration;
+pub mod routes;
+pub mod startup;
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer};
 use std::net::TcpListener;
-#[derive(serde::Deserialize)]
-struct FormData {
-    emial : String,
-    name : String 
-}
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-async fn subscribe(_form:web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
+
+use actix_web::{web, App, HttpResponse, HttpServer};
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
+            .route("/health_check", web::get().to(routes::health_check))
+            .route("/subscriptions", web::post().to(routes::subscribe))
     })
     .listen(listener)?
     .run();
